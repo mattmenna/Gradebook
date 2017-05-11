906601a9cde99796b544158d2aae43a62f8abc54
Matthew Menna <mattmenna@gmail.com>
2017-05-11 14:52:05 -0400

    Removed inheritance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,21 @@
 package com.company;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
         System.out.println("Gradebook Example");
+        try {
+            File gradebook = new File("C:\\Users\\Matt\\IdeaProjects\\Gradebook");
+            if (gradebook.createNewFile())
+        }
         new Course("algebra");
         new Student("matthew", "menna");
         new Assignment(20, 0);
+
+
     }
 }
