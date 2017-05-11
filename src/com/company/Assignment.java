f95a3cb68697dcce245cb573fc655d275876eda9
Matthew Menna <mattmenna@gmail.com>
2017-05-11 14:51:56 -0400

    Removed inheritance

--- src/com/company/Assignment.java
+++ src/com/company/Assignment.java
@@ -3,13 +3,10 @@ package com.company;
 /**
  * Created by Matt on 5/11/2017.
  */
-public class Assignment extends Course{
-    private final int ptsPossible;
-    private final int ptsReceived;
+public class Assignment {
 
-    public Assignment(String courseName, int ptsPossible, int ptsReceived) {
-        super(courseName);
-        this.ptsPossible = ptsPossible;
-        this.ptsReceived = ptsReceived;
+    public Assignment(int ptsPossible, int ptsReceived) {
+        System.out.println(ptsPossible);
+        System.out.println(ptsReceived);
     }
 }
