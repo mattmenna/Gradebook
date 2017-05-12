bf75ade6c61e8e56e3f55e84045bcd5994d6d34a
Matthew Menna <mattmenna@gmail.com>
2017-05-12 09:53:11 -0400

    Fix to bufferwrite

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,27 @@
 package com.company;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 	// write your code here
+        FileWriter filewrite;
+        BufferedWriter bufferwrite;
+
+        String courseName = "algebra";
+        String lastName = "menna";
+        String FirstName = "matthew";
+        String pathName = "C:\\Users\\Matt\\IdeaProjects\\Gradebook\\Gradebook.txt";
+
         System.out.println("Gradebook Example");
-        try {
-            File gradebook = new File("C:\\Users\\Matt\\IdeaProjects\\Gradebook");
-            if (gradebook.createNewFile())
-        }
+        File gradebook = new File(pathName);
         new Course("algebra");
         new Student("matthew", "menna");
         new Assignment(20, 0);
 
-
+        bufferwrite = new BufferedWriter(new FileWriter(pathName));
+        bufferwrite.write("test");
     }
+
 }
